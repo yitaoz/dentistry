d58f8d7f66b221613f657914d37a9e053e4e94df
Yitao Zhang <Yitao@rescomp-13-278522.stanford.edu>
2014-06-10 14:58:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DentistrySite::Application.config.secret_key_base = 'cbd4d5033af242a23a522f3397e9666c68fc326d9423448c6bf307505f6f02b8e081fc7f1511b4e095a891aec53c4316f3e33b09cc0304fa9c8952749389bf6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DentistrySite::Application.config.session_store :cookie_store, key: '_DentistrySite_session'
