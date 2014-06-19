d788e65f872531d9d032ddaa8b1fdf4c9f83e468
Yitao Zhang <Yitao@Yitaos-MacBook-Air.local>
2014-06-19 00:07:28 -0400

    third try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,14 +4,14 @@ DentistrySite::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['dentistryid'],
-    :access_key_id => ENV['AKIAJYJOILFF3A6DHKHQ'],
-    :secret_access_key => ENV['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']
+config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
